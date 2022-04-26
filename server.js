9ec04104fa2320a7bba06393c6b049ac83686ad4
CT <tellezc007@gmail.com>
2022-04-26 15:32:10 -0700

    add base files boilerplate

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const routes = require('./routes/index.js');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(routes);
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
