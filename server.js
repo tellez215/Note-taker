284e903a4bd47dd04aecba6e7f2cf72095a9835c
CT <tellezc007@gmail.com>
2022-04-27 21:15:00 -0700

    add comments

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// API routes
 app.use(routes);
 
 // Listens for the port 3001 
