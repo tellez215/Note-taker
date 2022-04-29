54854aeb2a86f38393499b87243daa14efbeb0b8
CT <tellezc007@gmail.com>
2022-04-28 17:31:23 -0700

    add uuidv4 and npm install to generate id

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
 const fs = require('fs')
-const uuid = require('../../id')
+const { uuid } = require('uuidv4')
 
 // GET method to request data from the saved notes
 routes.get('/notes', (req, res) => {
@@ -15,16 +15,20 @@ routes.post('/notes', (req, res) => {
     const db = fs.readFileSync('db/db.json')
     const pareDb = JSON.parse(db)
     const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuid()
-    }
-
-    // since its an array , I used the PUSH method to push newNote into the db.json file
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid()
+        }
+        // since its an array , I used the PUSH method to push newNote into the db.json file
     pareDb.push(newNote);
     fs.writeFileSync('db/db.json', JSON.stringify(pareDb))
     res.json(pareDb);
 })
 
+
+// routes.delete('/notes/:id', (req, res) => {
+
+
+// });
 
 module.exports = routes;
